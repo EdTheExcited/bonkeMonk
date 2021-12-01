eb61de566d265264bcab6563701008ca2ffb4f27
EdTheExcited <61635507+EdTheExcited@users.noreply.github.com>
2021-12-01 08:55:57 +0100

    first comit

--- bonkeMonk/Program.cs
+++ bonkeMonk/Program.cs
@@ -13,24 +13,32 @@ namespace bonkeMonk
                 RestClient client = new RestClient("https://pokeapi.co/api/v2/");
                 string searchAnswer = "";
                 Console.WriteLine("Welcome to a Pokemon Database, made to simplify your playthrough\ncreated by: Edvin Forsling");
-                Console.ReadLine();
-                Console.WriteLine("Search for desired Pokémon below:");
+                Console.WriteLine("\nSearch for desired Pokémon:");
+                Console.CursorTop -= 1;
+                Console.CursorLeft += 28;
                 searchAnswer = Console.ReadLine().Trim().ToLower();
-
                 RestRequest request = new RestRequest($"pokemon/{searchAnswer}");
 
                 IRestResponse response = client.Get(request);
 
                 // System.Console.WriteLine(response.Content);
+                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    Console.WriteLine("Error 404: Not Found!\n Press Enter To Continue");
+                }
+                else
+                {
 
-                Pokemon ditto = JsonSerializer.Deserialize<Pokemon>(response.Content);
+                    Pokemon ditto = JsonSerializer.Deserialize<Pokemon>(response.Content);
+                    ditto.PrintStat("speed");
+                    ditto.PrintStat("attack");
+                    ditto.PrintStat("special-attack");
+                    ditto.PrintStat("defense");
+                    ditto.PrintStat("special-defense");
+                    ditto.PrintStat("hp");
+                }
 
-                ditto.PrintStat("speed");
-                ditto.PrintStat("attack");
-                ditto.PrintStat("special-attack");
-                ditto.PrintStat("defense");
-                ditto.PrintStat("special-defense");
-                ditto.PrintStat("hp");
+
 
 
 
